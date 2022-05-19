e58fed33518e74c9c8aff58c485e0d2b39fa315b
Achraf Bounane <achraf.bounane@gs1fr.org>
2022-05-19 15:29:48 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {LoginService} from "./services/login.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-ref';
+  constructor(public loginService: LoginService) {
+    this.loginService.getSTO().subscribe((res)=>{
+      this.loginService.login(res.init_token).subscribe((ress)=>{
+        console.log(ress)
+      })
+    })
+  }
 }
